2102ebab99732558afa966abbf292b41a90ac5e3
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2024-03-20 17:54:26 +0000

    [#388] - remove climate warning

--- custom_components/versatile_thermostat/const.py
+++ custom_components/versatile_thermostat/const.py
@@ -329,7 +329,7 @@ CONF_WINDOW_ACTIONS = [
     CONF_WINDOW_ECO_TEMP,
 ]
 
-SUPPORT_FLAGS = ClimateEntityFeature.TARGET_TEMPERATURE
+SUPPORT_FLAGS = ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.TURN_OFF | ClimateEntityFeature.TURN_ON
 
 SERVICE_SET_PRESENCE = "set_presence"
 SERVICE_SET_PRESET_TEMPERATURE = "set_preset_temperature"
